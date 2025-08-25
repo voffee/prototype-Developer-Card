c39e187d5bf31d1e8352390169691dcae6e52ba7
Val <60014136+voffee@users.noreply.github.com>
2025-08-25 15:37:59 +0800

    Styling for cards completed, basic JS done. Positioning done.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 
 // Declare Variables
+const card = document.querySelector('.card-container');
+// const cardFront = document.querySelector('.card-obverse');
+// const cardBack = document.querySelector('.card-reverse');
 
 // Create Event Listeners
+cardFront.addEventListener('click', rotateCard);
+cardBack.addEventListener('click', rotateCard);
 
 // Create Functions
+function rotateCard(e) {
+    console.log(e);
+}
 
 // TEST FUNCTION
 // alert("test");
