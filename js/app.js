8700fe7bc86f395830afca1ca371a0a4bac317e4
Val <60014136+voffee@users.noreply.github.com>
2025-08-27 07:37:30 +0800

    Flipping animation completed.

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@ const card = document.querySelector('.card-container');
 // const cardBack = document.querySelector('.card-reverse');
 
 // Create Event Listeners
-cardFront.addEventListener('click', rotateCard);
-cardBack.addEventListener('click', rotateCard);
+// cardFront.addEventListener('click', rotateCard);
+// cardBack.addEventListener('click', rotateCard);
+card.addEventListener('click', rotateCard);
 
 // Create Functions
 function rotateCard(e) {
-    console.log(e);
+    // console.log(e);
+    console.log(e.currentTarget.classList);
+    e.currentTarget.classList.toggle('is-flipped');
 }
 
 // TEST FUNCTION
